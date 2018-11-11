09f1b051d289230714f60f23ab97b07401b50521
Nathan Tornquist <Tornquist@live.com>
2018-11-11 10:25:58 -0500

    Test activation token requests

--- test/test-auth.js
+++ test/test-auth.js
@@ -20,34 +20,110 @@ describe('Auth', function() {
     user = await UserHelper.create()
   })
 
-  describe('Logging in', () => {
-    it('works', async () => {
-      let postData = querystring.stringify({
-        grant_type: 'password',
-        email: user.email,
-        password: user.password
-      })
+  let authRequest = async (queryOverride = {}, headerOverride = {}) => {
+    let query = {
+      grant_type: 'password',
+      email: user.email,
+      password: user.password
+    }
 
-      let response = await server.inject({
-        method: 'POST',
-        url: '/oauth/token',
-        payload: postData,
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Content-Length': postData.length
-        }
-      })
+    Object.keys(queryOverride).forEach(key =>
+      queryOverride[key] != null
+        ? query[key] = queryOverride[key]
+        : delete query[key]
+    )
 
-      response.statusCode.should.eq(200)
+    let postData = querystring.stringify(query)
 
-      payload = JSON.parse(response.payload)
+    let headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': postData.length
+    }
 
-      payload.user_id.should.be.a('number')
-      payload.creation.should.be.a('number')
-      payload.expiration.should.be.a('number')
-      payload.token.should.be.a('string')
-      payload.refresh.should.be.a('string')
+    Object.keys(headerOverride).forEach(key =>
+      headerOverride[key] != null
+        ? headers[key] = headerOverride[key]
+        : delete headers[key]
+    )
+
+    let response = await server.inject({
+      method: 'POST',
+      url: '/oauth/token',
+      payload: postData,
+      headers: headers
     })
+
+    response.payload = JSON.parse(response.payload)
+    return response
+  }
+
+  describe('Non refresh or password grants', () => {
+    it('is rejected', async () => {
+      let response = await authRequest({ grant_type: 'facebook' }, {})
+
+      response.statusCode.should.eq(400)
+      response.payload.message.should.eq('Invalid request payload input')
+    })
+  })
+
+  describe('Requesting an authentication token', () => {
+
+    describe('Rejections', () => {
+      it('is rejected if grant_type is missing', async () => {
+        let response = await authRequest({ grant_type: null }, {})
+
+        response.statusCode.should.eq(400)
+        response.payload.message.should.eq('Invalid request payload input')
+      })
+
+      it('is rejected if password is missing', async () => {
+        let response = await authRequest({ password: null }, {})
+
+        response.statusCode.should.eq(400)
+        response.payload.message.should.eq('Invalid request payload input')
+      })
+
+      it('is rejected if email is missing', async () => {
+        let response = await authRequest({ email: null }, {})
+
+        response.statusCode.should.eq(400)
+        response.payload.message.should.eq('Invalid request payload input')
+      })
+
+      it('is rejected if the content-type is not form urlencoded', async () => {
+        let response = await authRequest({}, { 'Content-Type': null })
+
+        response.statusCode.should.eq(400)
+        response.payload.message.should.eq('Invalid request payload JSON format')
+      })
+
+      it('is rejected if the password is incorrect', async () => {
+        let response = await authRequest({ password: 'Super wrong pass'}, {})
+
+        response.statusCode.should.eq(401)
+        response.payload.message.should.eq('Unauthorized')
+      })
+    })
+
+    describe('Success', async () => {
+      it('returns a valid authentication token and refresh token', async () => {
+        let response = await authRequest({}, {})
+
+        response.statusCode.should.eq(200)
+
+        response.payload.user_id.should.be.a('number')
+        response.payload.creation.should.be.a('number')
+        response.payload.expiration.should.be.a('number')
+        response.payload.token.should.be.a('string')
+        response.payload.refresh.should.be.a('string')
+      })
+    })
+  })
+
+  describe('Refreshing an activation token', async () => {
+    it('is rejected if the token has expired')
+
+    it('is rejected if the token is invalid')
   })
 
   after(async () => {
