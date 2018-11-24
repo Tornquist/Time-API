4be397ed75da5fb8da0c0f0c773512b1bf22d35d
Nathan Tornquist <Tornquist@live.com>
2018-11-23 20:59:35 -0500

    Allow event and range entries to be created and completed for categories

--- handlers/entries.js
+++ handlers/entries.js
@@ -7,19 +7,81 @@ exports.path = '/entries'
 const GET_DESCRIPTION = 'Fetch Entries'
 
 const POST_DESCRIPTION = 'Create New Entries'
+const START_ACTION = 'start'
+const STOP_ACTION = 'stop'
 
 const GET_HANDLER = async (request, h) => {
   return boom.notImplemented()
 }
 
+const VALIDATE_AND_LOAD_CATEGORY = async (userID, categoryID) => {
+  const UNAUTHORIZED = new Error("Unauthorized")
+  try {
+    let category = await Time.Category.fetch(categoryID)
+    let accountID = category.account_id
+
+    let account = await Time.Account.fetch(accountID)
+
+    let userAuthorized = account.userIDs.includes(userID)
+    if (!userAuthorized) throw UNAUTHORIZED
+
+    return category
+  } catch (err) {
+    switch (err) {
+      case UNAUTHORIZED:
+        throw boom.unauthorized()
+      case Time.Error.Data.NOT_FOUND:
+        throw boom.badRequest()
+      default:
+        throw boom.badImplementation()
+    }
+  }
+}
+
 const POST_HANDLER = async (request, h) => {
-  return boom.notImplemented()
+  let userID = request.auth.credentials.user_id
+  let categoryID = request.payload.category_id
+  let category = await VALIDATE_AND_LOAD_CATEGORY(userID, categoryID)
+
+  let type = request.payload.type
+  let action = request.payload.action
+
+  try {
+    let entry;
+    if (type === Time.Type.Entry.EVENT) {
+      entry = await Time.Entry.logFor(category)
+    } else if (action === START_ACTION) {
+      entry = await Time.Entry.startFor(category)
+    } else {
+      entry = await Time.Entry.stopFor(category)
+    }
+
+    return {
+      id: entry.id,
+      type: entry.type,
+      category_id: entry.props.category_id,
+      started_at: entry.props.started_at,
+      ended_at: entry.props.ended_at
+    }
+  } catch (err) {
+    switch (err) {
+      case Time.Error.Request.INVALID_ACTION:
+        throw boom.badRequest("Unable to perform the desired action at this time")
+      default:
+        throw boom.badImplementation()
+    }
+  }
 }
 
 const POST_PAYLOAD = joi.object().keys({
-  account_id: joi.number().integer().required(),
-  name: joi.string().required(),
-  parent_id: joi.number().integer()
+  category_id: joi.number().integer().required(),
+  type: joi.string().valid(Object.values(Time.Type.Entry)),
+  action: joi.string().valid(START_ACTION, STOP_ACTION)
+    .when('type', {
+      is: Time.Type.Entry.RANGE,
+      then: joi.required(),
+      otherwise: joi.forbidden()
+    })
 })
 
 exports.get = {
---
+++ test/test-entries.js
@@ -0,0 +1,257 @@
+require('dotenv').config()
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const should = chai.should()
+const uuid = require('uuid/v4')
+const querystring = require('querystring')
+
+// Initialize Time Core to seed DB as-needed
+const config = require('./setup/config')
+const Time = require('time-core')(config)
+
+const AccountHelper = require('./helpers/account')
+const CategoryHelper = require('./helpers/category')
+const UserHelper = require('./helpers/user')
+
+describe('Entries', function() {
+  let server;
+
+  let user, token, account;
+  let work, email, program, codeReview, archive
+  let userAlt, accountAlt, rootAlt;
+
+  before(async function() {
+    this.timeout(5000)
+
+    server = await require(process.env.PWD+'/server')
+
+    AccountHelper.link(Time)
+    CategoryHelper.link(Time)
+    UserHelper.link(Time)
+
+    user = await UserHelper.create()
+    token = await UserHelper.login(user, server)
+
+    account = await AccountHelper.create(user.user.id)
+
+    work = await CategoryHelper.create("Work", account)
+    email = await CategoryHelper.create("Email", account, work)
+    program = await CategoryHelper.create("Program", account, work)
+    codeReview = await CategoryHelper.create("Code Review", account, work)
+    archive = await CategoryHelper.create("Archive", account, work)
+
+    userAlt = await UserHelper.create()
+    accountAlt = await AccountHelper.create(userAlt.user.id)
+    rootAlt = await AccountHelper.getRootCategory(accountAlt)
+  })
+
+  describe('Creating', () => {
+    it('denies requests to unauthorized categories', async () => {
+      let response = await server.inject({
+        method: 'POST',
+        url: '/entries',
+        headers: { 'Authorization': `Bearer ${token}` },
+        payload: {
+          category_id: rootAlt.id,
+          type: 'event'
+        }
+      })
+      response.statusCode.should.eq(401)
+    })
+
+    it('denies requests to categories that do not exist', async () => {
+      let response = await server.inject({
+        method: 'POST',
+        url: '/entries',
+        headers: { 'Authorization': `Bearer ${token}` },
+        payload: {
+          category_id: 100000,
+          type: 'event'
+        }
+      })
+      response.statusCode.should.eq(400)
+    })
+
+    describe('Events', () => {
+      it('allows creating entries', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: work.id,
+            type: 'event'
+          }
+        })
+
+        response.statusCode.should.eq(200)
+        let payload = JSON.parse(response.payload)
+        payload.id.should.be.a('number')
+        payload.type.should.eq('event')
+        payload.category_id.should.eq(work.id)
+
+        payload.started_at.should.be.a('number')
+        let date = new Date(payload.started_at)
+
+        let timeDelta = (new Date()).getTime() - payload.started_at
+        // Within 100ms. Allow for drift between services
+        timeDelta.should.be.lessThan(100)
+      })
+
+      it('denies creating events with a start action', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: work.id,
+            type: 'event',
+            action: 'start'
+          }
+        })
+        response.statusCode.should.eq(400)
+      })
+
+      it('denies creating events with a stop action', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: work.id,
+            type: 'event',
+            action: 'stop'
+          }
+        })
+        response.statusCode.should.eq(400)
+      })
+    })
+
+    describe('Ranges', () => {
+      it('denies stopping a range when none are active for a category', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: email.id,
+            type: 'range',
+            action: 'stop'
+          }
+        })
+        response.statusCode.should.eq(400)
+      })
+
+      it('allows starting a range for a category', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: email.id,
+            type: 'range',
+            action: 'start'
+          }
+        })
+        response.statusCode.should.eq(200)
+        let payload = JSON.parse(response.payload)
+        payload.id.should.be.a('number')
+        payload.type.should.eq('range')
+        payload.category_id.should.eq(email.id)
+
+        payload.started_at.should.be.a('number')
+        should.not.exist(payload.ended_at)
+      })
+
+      it('denies starting a range when one is active for the category', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: email.id,
+            type: 'range',
+            action: 'start'
+          }
+        })
+        response.statusCode.should.eq(400)
+      })
+
+      it('allows starting a second range for a different category', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: program.id,
+            type: 'range',
+            action: 'start'
+          }
+        })
+        response.statusCode.should.eq(200)
+        let payload = JSON.parse(response.payload)
+        payload.id.should.be.a('number')
+        payload.type.should.eq('range')
+        payload.category_id.should.eq(program.id)
+
+        payload.started_at.should.be.a('number')
+        should.not.exist(payload.ended_at)
+      })
+
+      it('allows stopping a range when one is active for a category', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: program.id,
+            type: 'range',
+            action: 'stop'
+          }
+        })
+        response.statusCode.should.eq(200)
+        let payload = JSON.parse(response.payload)
+        payload.id.should.be.a('number')
+        payload.type.should.eq('range')
+        payload.category_id.should.eq(program.id)
+
+        // TODO: Update Time-Core to output consistent formats
+        //       this returns number when fresh and date on retrieval
+        payload.started_at.should.be.a('string')
+        payload.ended_at.should.be.a('number')
+      })
+
+      it('allows stopping another active range', async () => {
+        let response = await server.inject({
+          method: 'POST',
+          url: '/entries',
+          headers: { 'Authorization': `Bearer ${token}` },
+          payload: {
+            category_id: email.id,
+            type: 'range',
+            action: 'stop'
+          }
+        })
+        response.statusCode.should.eq(200)
+        let payload = JSON.parse(response.payload)
+        payload.id.should.be.a('number')
+        payload.type.should.eq('range')
+        payload.category_id.should.eq(email.id)
+
+        // TODO: Update Time-Core to output consistent formats
+        //       this returns number when fresh and date on retrieval
+        payload.started_at.should.be.a('string')
+        payload.ended_at.should.be.a('number')
+      })
+    })
+  })
+
+  after(async () => {
+    await AccountHelper.cleanup(account)
+    await UserHelper.cleanup(user)
+
+    await AccountHelper.cleanup(accountAlt)
+    await UserHelper.cleanup(userAlt)
+  })
+})
