fff6938c1e1659ee60b67447eaadb7f08e2396c5
Nathan Tornquist <Tornquist@live.com>
2019-11-27 07:41:05 -0600

    Allow import requests to be made with root as a true or grouping object

--- handlers/import.js
+++ handlers/import.js
@@ -14,29 +14,67 @@ const POST_RESPONSES = {
   }
 }
 
+const validateNames = (node, root = true) => {
+  // Only the top level name can be an empty string
+  let emptyName = node.name.length === 0
+  let validName = !emptyName || emptyName && root
+
+  let validChildren = node.children
+    .map((c) => validateNames(c, false))
+    .reduce((a, c) => a && c, true)
+
+  return validName && validChildren
+}
+
+const countAll = (group) => {
+  let childrenCounts = group.children
+    .map(countAll)
+    .reduce((acc, cur) => {
+      return {
+        categories: acc.categories + cur.categories,
+        events: acc.events + cur.events,
+        ranges: acc.ranges + cur.ranges,
+      }
+    }, { categories: 0, events: 0, ranges: 0 })
+
+  return {
+    categories: childrenCounts.categories + 1,
+    events: childrenCounts.events + group.events.length,
+    ranges: childrenCounts.ranges + group.ranges.length
+  }
+}
+
 const POST_HANDLER = async (request, h) => {
-  return { received: "YES" }
+  let tree = request.payload
+
+  let validNaming = validateNames(tree)
+  if (!validNaming) {
+    throw boom.badRequest('Only the root name can be empty')
+  }
+
+  let createRootCategory = tree.name.length !== 0
+  let completeRequest = countAll(tree)
+  if (!createRootCategory) { completeRequest.categories-- }
+
+  // Inject ID to track status
+  completeRequest.id = 15 // TODO: Real ID
+  return completeRequest
 }
 
 const POST_PAYLOAD = joi.object().keys({
-  groups: joi.array().items(
-    joi.object().keys({
-      name: joi.string().required(),
-      events: joi.array().items(joi.object({
-        started_at: joi.string().isoDate().required(),
-        started_at_timezone: joi.string().required()
-      })),
-      ranges: joi.array().items(joi.object({
-        started_at: joi.string().isoDate().required(),
-        started_at_timezone: joi.string().required(),
-        ended_at: joi.string().isoDate().required(),
-        ended_at_timezone: joi.string().required()
-      })),
-      children: joi.array().items(joi.link('#tree'))
-    }).id("tree")
-  )
-})
-
+  name: joi.string().required().allow(''),
+  events: joi.array().items(joi.object({
+    started_at: joi.string().isoDate().required(),
+    started_at_timezone: joi.string().required()
+  })),
+  ranges: joi.array().items(joi.object({
+    started_at: joi.string().isoDate().required(),
+    started_at_timezone: joi.string().required(),
+    ended_at: joi.string().isoDate().required(),
+    ended_at_timezone: joi.string().required()
+  })),
+  children: joi.array().items(joi.link('#tree'))
+}).id("tree")
 
 exports.post = {
   description: POST_DESCRIPTION,
