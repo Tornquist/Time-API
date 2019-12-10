1414ad85c57262a15b4ec91df53b173efe75e097
Nathan Tornquist <Tornquist@live.com>
2019-12-10 07:24:28 -0600

    Flatten and expand import tests

--- test/test-import.js
+++ test/test-import.js
@@ -24,6 +24,7 @@ describe('Import', function() {
   let server;
 
   let user, token;
+  let importID = null;
 
   before(async function() {
     this.timeout(5000)
@@ -39,121 +40,160 @@ describe('Import', function() {
     token = await UserHelper.login(user, server)
   })
 
-  describe('Importing data', () => {
+  it('starts with no import requests in the account history', async () => {
+    let response = await server.inject({
+      method: 'GET',
+      url: `/import`,
+      headers: { 'Authorization': `Bearer ${token}` }
+    })
+    response.statusCode.should.eq(200)
+    response.payload = JSON.parse(response.payload)
+
+    response.payload.length.should.eq(0)
+  })
+
+  it('rejects malformed import requests', async () => {
+    let response = await server.inject({
+      method: 'POST',
+      url: '/import',
+      headers: { 'Authorization': `Bearer ${token}` },
+      payload: {
+        name: 'missing fields'
+      }
+    })
+    response.statusCode.should.eq(400)
+  })
+
+  it('rejects import with children with null titles', async () => {
+    let response = await server.inject({
+      method: 'POST',
+      url: '/import',
+      headers: { 'Authorization': `Bearer ${token}` },
+      payload: {
+        name: '',
+        events: [],
+        ranges: [],
+        children: [{
+          name: '', /* null child -- not allowed */
+          events: [],
+          ranges: [],
+          children: []
+        }]
+      }
+    })
+    response.statusCode.should.eq(400)
+  })
+
+  it('allows a large batch of data to be submitted', async () => {
+    let response = await server.inject({
+      method: 'POST',
+      url: '/import',
+      headers: { 'Authorization': `Bearer ${token}` },
+      payload: importData
+    })
+    response.statusCode.should.eq(200)
+    response.payload = JSON.parse(response.payload)
+
+    let res = response.payload
+
+    res.id.should.be.a('number')
+    importID = res.id      
+
+    res.created_at.should.be.a('string')
+    res.updated_at.should.be.a('string')
     
-    let importID = null;
+    res.categories.should.be.an('object')
+    res.categories.imported.should.eq(0)
+    res.categories.expected.should.eq(9)
 
-    it('allows a large batch of data to be submitted', async () => {
-      let response = await server.inject({
-        method: 'POST',
-        url: '/import',
-        headers: { 'Authorization': `Bearer ${token}` },
-        payload: importData
-      })
-      response.statusCode.should.eq(200)
-      response.payload = JSON.parse(response.payload)
+    res.entries.should.be.an('object')
+    res.entries.imported.should.eq(0)
+    res.entries.expected.should.eq(138)
 
-      let res = response.payload
+    res.complete.should.eq(false)
+    res.success.should.be.eq(false)
+  })
 
-      res.id.should.be.a('number')
-      importID = res.id      
+  it('allows checking import status periodically', async () => {
+    let lastCount = null;
 
-      res.created_at.should.be.a('string')
-      res.updated_at.should.be.a('string')
-      
-      res.categories.should.be.an('object')
-      res.categories.imported.should.eq(0)
-      res.categories.expected.should.eq(9)
-
-      res.entries.should.be.an('object')
-      res.entries.imported.should.eq(0)
-      res.entries.expected.should.eq(138)
-
-      res.complete.should.eq(false)
-      res.success.should.be.eq(false)
-    })
-
-    it('allows checking import status periodically', async () => {
-      let lastCount = null;
-
-      let checkCount = async () => {
-        let response = await server.inject({
-          method: 'GET',
-          url: `/import/${importID}`,
-          headers: { 'Authorization': `Bearer ${token}` }
-        })
-        response.statusCode.should.eq(200)
-        response.payload = JSON.parse(response.payload)
-        let payload = response.payload
-
-        if (lastCount === null) {
-          payload.categories.imported.should.be.lessThan(payload.categories.expected)
-          payload.entries.imported.should.be.lessThan(payload.entries.expected)
-
-          lastCount = payload.categories.imported + payload.entries.imported
-        } else {
-          let newCount = payload.categories.imported + payload.entries.imported
-          newCount.should.be.greaterThan(lastCount)
-
-          lastCount = newCount
-        }        
-      }
-
-      await checkCount()
-
-      for (let i = 0; i < 2; i++) {
-        await sleep(30)
-        await checkCount()
-      }
-    })
-
-    it('eventually returns success when import has completed', async () => {
-      let checkStatus = async () => {
-        let response = await server.inject({
-          method: 'GET',
-          url: `/import/${importID}`,
-          headers: { 'Authorization': `Bearer ${token}` }
-        })
-        response.statusCode.should.eq(200)
-        response.payload = JSON.parse(response.payload)
-        let payload = response.payload
-
-        if (payload.complete !== true) {
-          return null
-        }
-
-        return payload    
-      }
-
-      let notDone = true
-      let result = null
-
-      while (notDone) {
-        await sleep(10)
-        result = await checkStatus()
-
-        if (result !== null) {
-          notDone = false
-        }
-      }
-
-      result.complete.should.eq(true)
-      result.success.should.eq(true)
-      result.categories.imported.should.eq(result.categories.expected)
-      result.entries.imported.should.eq(result.entries.expected)
-    })
-
-    it('allows all requests to be fetched for a given user', async () => {
+    let checkCount = async () => {
       let response = await server.inject({
         method: 'GET',
-        url: `/import`,
+        url: `/import/${importID}`,
         headers: { 'Authorization': `Bearer ${token}` }
       })
       response.statusCode.should.eq(200)
       response.payload = JSON.parse(response.payload)
+      let payload = response.payload
 
-      response.payload.length.should.eq(1)
-      response.payload[0].id.should.eq(importID)
+      if (lastCount === null) {
+        payload.categories.imported.should.be.lessThan(payload.categories.expected)
+        payload.entries.imported.should.be.lessThan(payload.entries.expected)
+
+        lastCount = payload.categories.imported + payload.entries.imported
+      } else {
+        let newCount = payload.categories.imported + payload.entries.imported
+        newCount.should.be.greaterThan(lastCount)
+
+        lastCount = newCount
+      }        
+    }
+
+    await checkCount()
+
+    for (let i = 0; i < 2; i++) {
+      await sleep(30)
+      await checkCount()
+    }
+  })
+
+  it('eventually returns success when import has completed', async () => {
+    let checkStatus = async () => {
+      let response = await server.inject({
+        method: 'GET',
+        url: `/import/${importID}`,
+        headers: { 'Authorization': `Bearer ${token}` }
+      })
+      response.statusCode.should.eq(200)
+      response.payload = JSON.parse(response.payload)
+      let payload = response.payload
+
+      if (payload.complete !== true) {
+        return null
+      }
+
+      return payload    
+    }
+
+    let notDone = true
+    let result = null
+
+    while (notDone) {
+      await sleep(10)
+      result = await checkStatus()
+
+      if (result !== null) {
+        notDone = false
+      }
+    }
+
+    result.complete.should.eq(true)
+    result.success.should.eq(true)
+    result.categories.imported.should.eq(result.categories.expected)
+    result.entries.imported.should.eq(result.entries.expected)
+  })
+
+  it('allows all requests to be fetched for a given user', async () => {
+    let response = await server.inject({
+      method: 'GET',
+      url: `/import`,
+      headers: { 'Authorization': `Bearer ${token}` }
     })
+    response.statusCode.should.eq(200)
+    response.payload = JSON.parse(response.payload)
+
+    response.payload.length.should.eq(1)
+    response.payload[0].id.should.eq(importID)
   })
 })
